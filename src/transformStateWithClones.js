5fd960c86a4e2fc1d59de6b183ebd29dbd4375e2
Anton Olifan <olifananton@gmail.com>
2024-12-10 11:34:23 +0100

    Rewrite solution to switch

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -12,17 +12,24 @@ function transformStateWithClones(state, actions) {
   const arrayStates = [];
 
   for (const action of actions) {
-    if (action.type === 'clear') {
-      copyState = {};
-    } else if (action.type === 'addProperties') {
-      copyState = { ...copyState, ...action.extraData };
-    } else if (action.type === 'removeProperties') {
-      copyState = { ...copyState };
+    switch (action.type) {
+      case 'clear':
+        copyState = {};
+        break;
 
-      for (const key of action.keysToRemove) {
-        delete copyState[key];
-      }
+      case 'addProperties':
+        copyState = { ...copyState, ...action.extraData };
+        break;
+
+      case 'removeProperties':
+        copyState = { ...copyState };
+
+        for (const key of action.keysToRemove) {
+          delete copyState[key];
+        }
+        break;
     }
+
     arrayStates.push(copyState);
   }
 
